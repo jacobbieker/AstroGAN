359542e8c9e9be5d85016be49cd2861ff7a12b06
Jacob Bieker <jacob@bieker.tech>
2019-05-14 12:11:12 +0200

    GAN that run on SDSS data
    Not sure if working correctly, but is going okay

--- sdss_gan.py
+++ sdss_gan.py
@@ -6,27 +6,29 @@ GAN for generating new galaxies from SDSS images, taken from Kaggle's Galaxy Zoo
 
 from __future__ import print_function, division
 
-from keras.datasets import mnist
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
-
+from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
 
 import numpy as np
 
+
 class DCGAN():
-    def __init__(self, width=424, height=424, channels=3):
+    def __init__(self, width=424, height=424, channels=3, directory="", batch_size=32):
         # Input shape
         self.img_rows = width
         self.img_cols = height
         self.channels = channels
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
         self.latent_dim = 500
+        self.directory = directory
+        self.batch_size = batch_size
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -54,12 +56,26 @@ class DCGAN():
         self.combined = Model(z, valid)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
+    def datagen(self, directory):
+        """
+        Generates the images for the GAN from the SDSS ones
+
+        :param directory:
+        :return:
+        """
+        datagen = ImageDataGenerator(rescale=1./127.5, horizontal_flip=True, vertical_flip=True,
+                                     #rotation_range=180, height_shift_range=0.25, width_shift_range=0.25,
+                                     cval=0., fill_mode='constant')
+        generator = datagen.flow_from_directory(directory, target_size=(self.img_rows,self.img_cols), batch_size=self.batch_size, shuffle=True,
+                                                class_mode=None)
+        return generator
+
     def build_generator(self):
 
         model = Sequential()
 
-        model.add(Dense(512 * 7 * 7, activation="relu", input_dim=self.latent_dim))
-        model.add(Reshape((7, 7, 512)))
+        model.add(Dense(128 * 26 * 26, activation="relu", input_dim=self.latent_dim))
+        model.add(Reshape((26, 26, 128)))
         model.add(UpSampling2D())
         model.add(Conv2D(128, kernel_size=3, padding="same"))
         model.add(BatchNormalization(momentum=0.8))
@@ -108,18 +124,20 @@ class DCGAN():
 
         return Model(img, validity)
 
-    def train(self, epochs, batch_size=128, save_interval=50):
+    def train(self, epochs, save_interval=50):
 
         # Load the dataset
-        (X_train, _), (_, _) = mnist.load_data()
+        # Load it as a generator
+        image_gen = self.datagen(self.directory)
+        #(X_train, _), (_, _) = mnist.load_data()
 
         # Rescale -1 to 1
-        X_train = X_train / 127.5 - 1.
-        X_train = np.expand_dims(X_train, axis=3)
+        #X_train = X_train / 127.5 - 1.
+        #X_train = np.expand_dims(X_train, axis=3)
 
         # Adversarial ground truths
-        valid = np.ones((batch_size, 1))
-        fake = np.zeros((batch_size, 1))
+        valid = np.ones((self.batch_size, 1))
+        fake = np.zeros((self.batch_size, 1))
 
         for epoch in range(epochs):
 
@@ -127,12 +145,13 @@ class DCGAN():
             #  Train Discriminator
             # ---------------------
 
-            # Select a random half of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
+            # Select random images from batch_size
+            imgs = image_gen.next()
+            # Rescaled between 1 and -1
+            imgs = imgs - 1.
 
             # Sample noise and generate a batch of new images
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            noise = np.random.normal(0, 1, (self.batch_size, self.latent_dim))
             gen_imgs = self.generator.predict(noise)
 
             # Train the discriminator (real classified as ones and generated as zeros)
@@ -169,10 +188,10 @@ class DCGAN():
                 axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/mnist_%d.png" % epoch)
+        fig.savefig("images/sdss_%d.png" % epoch)
         plt.close()
 
 
 if __name__ == '__main__':
-    dcgan = DCGAN()
-    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
+    dcgan = DCGAN(width=104, batch_size=128, height=104, directory="data/")
+    dcgan.train(epochs=4000, save_interval=50)
